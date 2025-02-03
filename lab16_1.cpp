8792d618b94a37c10378efad7e24a76a6ec2c06e
liarles <62631264+liarles@users.noreply.github.com>
2025-02-03 16:45:08 +0700

    Update lab16_1.cpp

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,34 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void showData(double *dPtr,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(dPtr+i);
+        if((i+1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+void randData(double *dPtr,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(dPtr+i) = (rand()%101)*0.01;
+    }
+}
+void findRowSum(const double *dPtr, double *sum1, int N, int M) {
+    for (int i = 0; i < N; i++) { 
+        *sum1 = 0;
+        for (int j = 0; j < M; j++) {
+            *sum1 += *(dPtr + i * M + j);
+        }
+        sum1++;
+    }
+}
+void findColSum(const double *dPtr, double *sum2, int N, int M) {
+    for (int i = 0; i < M; i++) {
+        *sum2 = 0;
+        for (int j = 0; j < N; j++) {  
+            *sum2 += *(dPtr + j * M + i);  
+        }
+        sum2++;
+    }
+}
